499c3acffc1003cdba178e46788ba1ea564ad6a0
fabipalta <128632623+fabipalta@users.noreply.github.com>
2023-04-05 16:54:14 -0400

    Update import math.py

--- import math.py
+++ import math.py
@@ -1,6 +1,6 @@
 
 import math
-print("ingrese ekl radio del circulo:")
+print("ingrese el radio del circulo:")
 radio = float(input())
 circunferencia = math.pi * (radio* radio)
 print("el area del circulo con radio", radio, " es:", round(circunferencia,6))
